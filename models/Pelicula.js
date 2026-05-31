be6e38be444d974d284b9cbd437b89384c6558ef
mariaosuna-fic <taquitomaria@gmail.com>
2026-05-30 23:24:34 -0700

    Create Pelicula.js

---
+++ models/Pelicula.js
@@ -0,0 +1,15 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../database.js';
+
+const Pelicula = sequelize.define('Pelicula', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    titulo: {type: DataTypes.STRING, allowNull: false},
+    director: {type: DataTypes.STRING, allowNull: false},
+    año: {type: DataTypes.INTEGER, allowNull: false},
+    genero: {type: DataTypes.STRING, allowNull: false},
+    sinopsis: {type: DataTypes.TEXT, allowNull: false}
+}, {
+    timestamps: false
+});
+
+export default Pelicula;
